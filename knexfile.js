6859c54bb78bb0caf3dbd27cb025af1e869edb69
Vasco Fernandes <vasco.box@gmail.com>
2021-12-06 11:11:07 +0000

    Scaffolding
    Initial configuration files and project structure

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+module.exports = {
+  test: {
+    client: "postgresql",
+    connection: {
+      database: "limbictest",
+      user: "vasco",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "limbic",
+      user: "vasco",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
