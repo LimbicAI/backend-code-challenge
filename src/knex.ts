db32c49a378241dac3d6527c9aaf1bd3a4baea2e
Vasco Fernandes <vasco.box@gmail.com>
2021-12-06 11:12:29 +0000

    Database structure
    Knex migration files for db schema and knex connection helper.

---
+++ src/knex.ts
@@ -0,0 +1,4 @@
+import knex from "knex";
+const config = require("../knexfile");
+
+export default knex(config[process.env.NODE_ENV ?? "development"]);
---
+++ src/migrations/20211205201350_init.ts
@@ -0,0 +1,27 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable("users", t => {
+    t.uuid("id")
+      .notNullable()
+      .defaultTo(knex.raw("gen_random_uuid()"))
+      .primary();
+    t.text("handle").notNullable();
+  });
+  await knex.schema.createTable("posts", t => {
+    t.uuid("id")
+      .notNullable()
+      .defaultTo(knex.raw("gen_random_uuid()"))
+      .primary();
+    t.text("title").notNullable();
+    t.text("text").notNullable();
+    t.uuid("author_id").notNullable();
+    t.timestamps(true, true);
+
+    t.foreign("author_id").references("users.id");
+  });
+}
+export async function down(knex: Knex) {
+  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists("posts");
+}
