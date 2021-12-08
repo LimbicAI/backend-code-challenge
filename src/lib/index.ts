423d27b90ea6aef0998601463326f624ef2563fb
Vasco Fernandes <vasco.box@gmail.com>
2021-12-08 08:58:05 +0000

    Docker compose
    Add docker compose connecting to a postgres DB. Also adds logic to wait
    some time, configured through an env variable, before running migrations
    on the DB.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,28 @@
-import server from "./server";
+import { join } from "path";
+import knex from "../knex";
+import serverFactory from "./server";
 
-server();
+async function init() {
+  if (
+    process.env.MIGRATE_LATEST &&
+    /^true$/i.test(process.env.MIGRATE_LATEST)
+  ) {
+    await knex.migrate.latest({
+      directory: join(__dirname, `../migrations`),
+    });
+  }
+
+  const server = await serverFactory();
+  const { url } = await server.listen();
+  console.log(`🚀  Server ready at ${url}`);
+}
+
+const delayStart = process.env.DELAY_START_MS
+  ? parseInt(process.env.DELAY_START_MS)
+  : 0;
+
+if (delayStart) {
+  setTimeout(init, delayStart);
+} else {
+  init();
+}
