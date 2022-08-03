9e432e5721e4271ee1c0e25ae4209bbe45d0ec3b
Ijaz Ul Rehman <ijazulrehmannajeeb@gmail.com>
2022-08-03 16:31:06 +0500

    feat: add tests for posts resolver and service

--- src/posts/posts.resolver.spec.ts
+++ src/posts/posts.resolver.spec.ts
@@ -1,13 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/entities/user.entity';
 import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
 
 describe('PostsResolver', () => {
   let resolver: PostsResolver;
+  let mockPosts = [
+    {
+      id: 1,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    },
+    {
+      id: 2,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    },
+    {
+      id: 3,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    }
+  ]
+  let mockPostService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsResolver, PostsService],
+      providers: [
+        PostsResolver,
+        {
+          provide: PostsService,
+          useValue: mockPostService
+        }
+      ],
     }).compile();
 
     resolver = module.get<PostsResolver>(PostsResolver);
@@ -15,5 +53,33 @@ describe('PostsResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('createPost', async () => {
+    const post = mockPosts[0];
+    mockPostService.create.mockReturnValue(post);
+
+    const { user, text } = post;
+    const res = await resolver.createPost(user as User, { text });
+    expect(res).toMatchObject(post);
+    expect(mockPostService.create).toHaveBeenCalledWith(user.id, { text });
+  });
+
+  it('findAll', async () => {
+    mockPostService.findAll.mockReturnValue(mockPosts);
+
+    const res = await resolver.findAll();
+    expect(res).toMatchObject(mockPosts);
+    expect(mockPostService.findAll).toHaveBeenCalled();
+
+  });
+
+  it('findOne', async () => {
+    const post = mockPosts[0];
+    mockPostService.findOne.mockReturnValue(post)
+
+    const res = await resolver.findOne(post.id);
+    expect(res).toMatchObject(post);
+    expect(mockPostService.findOne).toHaveBeenCalledWith(post.id);
   });
 });
--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let service: PostsService;
-
+  let mockPosts = [
+    {
+      id: 1,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    },
+    {
+      id: 2,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    },
+    {
+      id: 3,
+      text: "ut aspernatur corporis harum nihil quis provident sequi\\nmollitia nobis aliquid molestiae\\nperspiciatis et ea nemo ab reprehenderit accusantium quas\\nvoluptate dolores velit et doloremque molestiae",
+      user: {
+        id: 1,
+        email: 'test@test.com'
+      }
+    }
+  ]
+  let mockPostRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+    find: jest.fn()
+  }
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockPostRepository
+        }
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
@@ -14,5 +53,47 @@ describe('PostsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create', async () => {
+
+    const post = mockPosts[0];
+    mockPostRepository.create.mockReturnValue(post);
+    mockPostRepository.save.mockReturnValue(post);
+
+    const { user, text } = post;
+    const res = await service.create(user.id, { text });
+    expect(res).toMatchObject(post);
+    expect(mockPostRepository.create).toHaveBeenCalledWith({
+      user: {
+        id: user.id
+      },
+      text
+    });
+    expect(mockPostRepository.save).toHaveBeenCalledWith(post);
+  });
+
+  it('findAll', async () => {
+
+    mockPostRepository.find.mockReturnValue(mockPosts);
+
+    const res = await service.findAll();
+    expect(res).toMatchObject(mockPosts);
+    expect(mockPostRepository.find).toHaveBeenCalledWith({
+      relations: ['user']
+    });
+  });
+
+  it('findOne', async () => {
+
+    mockPostRepository.findOne.mockReturnValue(mockPosts[0]);
+
+    const { user, ...post } = mockPosts[0];
+    const res = await service.findOne(post.id);
+    expect(res).toMatchObject(mockPosts[0]);
+    expect(mockPostRepository.findOne).toHaveBeenCalledWith({
+      where: { id: post.id },
+      relations: ['user']
+    });
   });
 });
